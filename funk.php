ce52acd438e28fbc106ab03393c06740b425f11b
Jüri Kalbin <jkalbin@enos.itcollege.ee>
2017-05-01 10:28:54 +0300

    Looamaed homework part1

---
+++ funk.php
@@ -0,0 +1,77 @@
+<?php
+
+
+function connect_db(){
+	global $connection;
+	$host="localhost";
+	$user="test";
+	$pass="t3st3r123";
+	$db="test";
+	$connection = mysqli_connect($host, $user, $pass, $db) or die("ei saa ühendust mootoriga- ".mysqli_error());
+	mysqli_query($connection, "SET CHARACTER SET UTF8") or die("Ei saanud baasi utf-8-sse - ".mysqli_error($connection));
+}
+
+function logi(){
+	// siia on vaja funktsionaalsust (13. nädalal)
+
+	include_once('views/login.html');
+}
+
+function logout(){
+	$_SESSION=array();
+	session_destroy();
+	header("Location: ?");
+}
+
+function kuva_puurid(){
+	global $connection;
+	// siia on vaja funktsionaalsust
+	$puurid=array();
+	$puuri_p2ring=mysqli_query($connection, "SELECT DISTINCT(puur) FROM 12103979_loomaaed ORDER BY puur ASC");
+	while($puuri_nr = mysqli_fetch_assoc($puuri_p2ring)){
+		$loom_puuris=$puuri_nr["puur"];
+		$looma_p2ring=mysqli_query($connection, "SELECT * FROM 12103979_loomaaed WHERE puur=$loom_puuris");
+		while($loomarida = mysqli_fetch_assoc($looma_p2ring)){
+			$puurid[$puuri_nr["puur"]][]=$loomarida;
+		}
+	}
+	include_once('views/puurid.html');
+}
+
+function lisa(){
+	// siia on vaja funktsionaalsust (13. nädalal)
+	
+	include_once('views/loomavorm.html');
+	
+}
+
+function upload($name){
+	$allowedExts = array("jpg", "jpeg", "gif", "png");
+	$allowedTypes = array("image/gif", "image/jpeg", "image/png","image/pjpeg");
+	$extension = end(explode(".", $_FILES[$name]["name"]));
+
+	if ( in_array($_FILES[$name]["type"], $allowedTypes)
+		&& ($_FILES[$name]["size"] < 100000)
+		&& in_array($extension, $allowedExts)) {
+    // fail õiget tüüpi ja suurusega
+		if ($_FILES[$name]["error"] > 0) {
+			$_SESSION['notices'][]= "Return Code: " . $_FILES[$name]["error"];
+			return "";
+		} else {
+      // vigu ei ole
+			if (file_exists("pildid/" . $_FILES[$name]["name"])) {
+        // fail olemas ära uuesti lae, tagasta failinimi
+				$_SESSION['notices'][]= $_FILES[$name]["name"] . " juba eksisteerib. ";
+				return "pildid/" .$_FILES[$name]["name"];
+			} else {
+        // kõik ok, aseta pilt
+				move_uploaded_file($_FILES[$name]["tmp_name"], "pildid/" . $_FILES[$name]["name"]);
+				return "pildid/" .$_FILES[$name]["name"];
+			}
+		}
+	} else {
+		return "";
+	}
+}
+
+?>
